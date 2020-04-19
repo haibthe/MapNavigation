9ed7fa18a61dea7d721c51e69b11de9774e1cd0b
HaiBui <thehai.bui@gmail.com>
2020-04-19 13:27:59 +0700

    [#2][#3] Create Project navigation and navigation-ui

---
+++ app/src/main/java/com/hb/map/navigation/app/MapNavigationApp.kt
@@ -0,0 +1,10 @@
+package com.hb.map.navigation.app
+
+import androidx.multidex.MultiDexApplication
+
+class MapNavigationApp : MultiDexApplication() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
